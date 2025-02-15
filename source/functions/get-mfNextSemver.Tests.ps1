3a847970b23ed48115bfaa982eba88ca2cc9f9d8
adrian.andersson <adrian.ghub@anderss.com>
2025-02-15 14:40:04 +1100

    test: get-mfnextSemver.Tests - Update to address new lowercase default prerelease label. Added a test to verify specifying new prerelease label manually

--- source/functions/get-mfNextSemver.Tests.ps1
+++ source/functions/get-mfNextSemver.Tests.ps1
@@ -8,11 +8,11 @@ Describe 'get-mfNextSemver' {
         $initialVersion = get-mfNextSemver -initialPreRelease
     }
     It 'should substantiate correct initial version' {
-        $initialVersion.ToString() | should -BeExactly '1.0.0-PREv001'
+        $initialVersion.ToString() | should -BeExactly '1.0.0-prev001'
     }
     It 'Should increment the prerelease version'{
         $next = get-mfNextSemver -version $initialVersion -prerelease
-        $next.ToString() | should -BeExactly '1.0.0-PREv002'
+        $next.ToString() | should -BeExactly '1.0.0-prev002'
     }
     It 'Should create a stable version'{
         $next = get-mfNextSemver -version $initialVersion -stableRelease
@@ -21,20 +21,25 @@ Describe 'get-mfNextSemver' {
     It 'Should increase the Patch version into a new PreRelease' {
         $next = get-mfNextSemver -version $initialVersion -stableRelease
         $newPreRelease = get-mfNextSemver -version $next -prerelease -increment Patch
-        $newPreRelease.ToString() | should -BeExactly '1.0.1-PREv001'
+        $newPreRelease.ToString() | should -BeExactly '1.0.1-prev001'
     }
     It 'Should increase the Minor version into a new PreRelease' {
         $next = get-mfNextSemver -version $initialVersion -stableRelease
         $newPreRelease = get-mfNextSemver -version $next -prerelease -increment Minor
-        $newPreRelease.ToString() | should -BeExactly '1.1.0-PREv001'
+        $newPreRelease.ToString() | should -BeExactly '1.1.0-prev001'
     }
     It 'Should increase the Major version into a new PreRelease'  {
         $next = get-mfNextSemver -version $initialVersion -stableRelease
         $newPreRelease = get-mfNextSemver -version $next -prerelease -increment Major
-        $newPreRelease.ToString() | should -BeExactly '2.0.0-PREv001'
+        $newPreRelease.ToString() | should -BeExactly '2.0.0-prev001'
     }
     It 'Should create a new patch Prerelease'  {
         $newPreRelease = get-mfNextSemver -version $([semver]::new('3.0.0')) -prerelease
-        $newPreRelease.ToString() | should -BeExactly '3.0.1-PREv001'
+        $newPreRelease.ToString() | should -BeExactly '3.0.1-prev001'
+    }
+
+    It 'Should create a new prerelease label and restart the prerelease numbering'  {
+        $newPreRelease = get-mfNextSemver -version $([semver]::new('3.0.0')) -prerelease -preReleaseLabel 'stage'
+        $newPreRelease.ToString() | should -BeExactly '3.0.1-stagev001'
     }
 }
