403e113295354692a6fb111ec5f66692208268c6
adrian.andersson <adrian.ghub@anderss.com>
2025-07-02 01:24:58 +1000

    chore: New Prerelease to tes passthrough of release notes

--- build/ModuleForge/ModuleForge.psm1
+++ build/ModuleForge/ModuleForge.psm1
@@ -1,7 +1,7 @@
 <#
 Module created by ModuleForge
-	 ModuleForge Version: 1.1.1
-	BuildDate: 2025-07-02T00:58:03
+	 ModuleForge Version: 1.1.2
+	BuildDate: 2025-07-02T01:23:57
 #>
 function add-mfGithubScaffold
 {
@@ -327,7 +327,11 @@ function build-mfProject
         [switch]$exportEnums,
         #Use this to not put anything in nestedmodules, making everything a single file. By default validators are put in a separate nestedmodule script to ensure they are loaded properly
         [Parameter()]
-        [switch]$noExternalFiles
+        [switch]$noExternalFiles,
+        [Parameter()]
+        [string]$releaseNotes,
+        [Parameter()]
+        [switch]$includeReleaseNotesInDescription
 
         
     )
@@ -682,6 +686,16 @@ function build-mfProject
             Guid = $config.guid
             PowershellVersion = $config.minimumPsVersion.tostring()
             CmdletsToExport = [array]@()
+        }
+
+        if($releaseNotes)
+        {
+            #Add the release notes if they were included
+            $splatManifest.releaseNotes = $releaseNotes
+            if($includeReleaseNotesInDescription)
+            {
+                $splatManifest.Description = "$($config.Description)`n`n$($releaseNotes)"
+            }
         }
         #Add the extra bits if present
         #Splatting really doesn't like nulls
