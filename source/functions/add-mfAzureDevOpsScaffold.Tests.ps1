98f9fc8566bdbdda1aa8696ba04885c38ca1f770
adrian.andersson <adrian.ghub@anderss.com>
2025-07-06 01:06:49 +1000

    feat: added add-mfAzureDevOpsScaffold function and tests

---
+++ source/functions/add-mfAzureDevOpsScaffold.Tests.ps1
@@ -0,0 +1,97 @@
+BeforeAll{
+
+    #Reference Current Path
+    $currentPath = $(get-location).path
+    $sourcePath = join-path -path $currentPath -childPath 'source'
+
+    $tempCopyLocation = join-path -Path $currentPath -ChildPath 'TempCopy'
+
+    $privatePath = join-path $sourcePath -ChildPath 'private'
+
+    $mockPsScriptRoot = $sourcePath
+
+    #Create a temp folder so we don't clobber anything
+    $testPath = join-path -path $currentPath -childPath 'azdScaffoldTest'
+    if(!(test-path $testPath)){
+        new-Item -ItemType Directory -Path $testPath
+    }
+
+    Set-Location $testPath
+    #Our function checks for a moduleForgeConfig, but doesn't check the contents
+    #Creating an empty file should suffice
+    new-Item -ItemType File -Name 'moduleForgeConfig.xml'
+    
+    #Some references to make things easier
+    $azdFolder = join-path -Path $testPath -ChildPath '.azuredevops'
+    $prTemplateFile = join-path $azdFolder 'pull_request_template.md'
+    $pipelinesFolder = join-path $azdFolder 'pipelines'
+    $pesterTestFile = join-path $pipelinesFolder 'pesterTest.yml'
+    $buildandreleaseFile = join-path $pipelinesFolder 'buildandrelease.yml'
+
+
+     #Load This File
+    . $PSCommandPath.Replace('.Tests.ps1','.ps1')
+
+    
+}
+
+Describe 'add-mfAzureDevOpsScaffold should Copy Files' {
+    BeforeAll{
+        add-mfAzureDevOpsScaffold
+    }
+
+    It 'Should have created the .azuredevops folder' {
+        get-Item $azdFolder|Should -Not -BeNullOrEmpty
+    }
+    It 'Should have created the  PR Template' {
+        get-Item $prTemplateFile|Should -Not -BeNullOrEmpty
+        get-content $prTemplateFile|should -Not -BeNullOrEmpty
+    }
+    It 'Should have created the pipelines folder '  {
+        get-Item $pipelinesFolder|Should -Not -BeNullOrEmpty
+    }
+    It 'Should have created the pesterTest file '  {
+        get-Item $pesterTestFile|Should -Not -BeNullOrEmpty
+        get-content $pesterTestFile|should -Not -BeNullOrEmpty
+    }
+    It 'Should have created the buildandrelease file '  {
+        get-Item $buildandreleaseFile|Should -Not -BeNullOrEmpty
+        get-content $buildandreleaseFile|should -Not -BeNullOrEmpty
+    }
+
+}
+
+Describe 'add-mfAzureDevOpsScaffold should NOT copy if files exist' {
+    BeforeAll{
+        $endOfFileString = "`n#### End of File Change"
+        $endOfFileString|Out-File $prTemplateFile -Append
+        add-mfAzureDevOpsScaffold
+    }
+    
+    It 'Rerunning should not remove our end of file message in the PR Template'  {
+        get-Item $prTemplateFile|Should -Not -BeNullOrEmpty
+        get-content $prTemplateFile|should -Not -BeNullOrEmpty
+        get-content $prTemplateFile|should -Contain '#### End of File Change'
+    }
+
+}
+
+Describe 'add-mfAzureDevOpsScaffold should copy if files exist and -force is used' {
+
+    BeforeAll{
+        add-mfAzureDevOpsScaffold -force
+    }
+    
+    It 'Rerunning should not remove our end of file message in the PR Template'  {
+        get-Item $prTemplateFile|Should -Not -BeNullOrEmpty
+        get-content $prTemplateFile|should -Not -BeNullOrEmpty
+        get-content $prTemplateFile|should -Not  -Contain '#### End of File Change'
+    }
+
+}
+
+AfterAll{
+    Remove-Variable mockPsScriptRoot -ErrorAction Ignore
+    Set-Location $currentPath
+    remove-Item $testPath -Force -Recurse
+}
---
+++ source/functions/add-mfAzureDevOpsScaffold.ps1
@@ -0,0 +1,120 @@
+function add-mfAzureDevOpsScaffold
+{
+
+    <#
+        .SYNOPSIS
+            Initialises a `.azuredevops` scaffold in a PowerShell module, including YAML Azure DevOps Pipelines for pester testing and build and release
+
+            
+        .DESCRIPTION
+            This function will create a '.azuredevops' folder in the moduleforge root (if one does not exist).
+            It will create 2 Azure DevOps Pipelines files, 1 for Pester testing, 1 for buildAndRelease
+            It will create 1 Pull Request template.
+
+            The buildAndRelease pipeline will use Git Tags to mark versions. If you stick with this template you should refrain from
+            using tags for other purposes.
+            
+            The purpose of these workflows and templates is to get you started, creating a quick and easy workflow scaffold. 
+            Please feel free to change the workflows and template to your own needs and preferences.
+            
+        .EXAMPLE
+            add-mfAzureDevOpsScaffold
+
+            #### DESCRIPTION
+            Copies the `.azuredevops` folder from the module's `resource` directory to the current module, skipping existing files.
+
+            #### OUTPUT
+            Should have a .azuredevops folder, with pipelines and a PR template
+
+        .EXAMPLE
+            add-mfAzureDevOpsScaffold -Force
+
+            #### DESCRIPTION
+            Copies the `.azuredevops` scaffold and overwrites existing files in `.azuredevops` directory
+
+            #### OUTPUT
+            Should have a .azuredevops folder, with workflows and a PR template
+            
+        .NOTES
+            Author: Adrian Andersson
+            
+    #>
+
+    [CmdletBinding()]
+    PARAM(
+        #Root path of the module. Uses the current working directory by default. Aliased path, but use modulePath as paramname to avoid confusion
+        [Parameter()]
+        [alias('path')]
+        [string]$modulePath = $(get-location).path,
+        #Module Config reference
+        [Parameter(DontShow)]
+        [string]$configFile = 'moduleForgeConfig.xml',
+        #Azure DevOps folder
+        [Parameter(DontShow)]
+        [string]$azdFolder = '.azuredevops',
+        #Should we overwrite if files exist?
+        [switch]$force
+    )
+    begin{
+        #Return the script name when running verbose, makes it tidier
+        write-verbose "===========Executing $($MyInvocation.InvocationName)==========="
+        #Return the sent variables when running debug
+        Write-Debug "BoundParams: $($MyInvocation.BoundParameters|Out-String)"
+        if($mockPsScriptRoot)
+        {
+            #Assume we are in pester test, and use the $mockPsScriptRoot param
+            write-warning 'Assuming PSScriptRoot from $mockPsScriptRoot. This should only be done for testing'
+            $resourceFolder = Join-Path $mockPsScriptRoot 'resource'
+            
+        }else{
+            $resourceFolder = Join-Path $PSScriptRoot 'resource'
+        }
+        write-verbose "ResourceFolder:  $resourceFolder"
+        
+        if(!(test-path  $resourceFolder))
+        {
+            throw 'Err: Unable to find resource folder in ModuleForge module'
+        }
+
+        $resourceFolderAzd = join-path $resourceFolder 'azureDevOps'
+        if(!(test-path  $resourceFolderAzd))
+        {
+            throw 'Err: Found resource folder, but not azureDevOps folder in ModuleForge module'
+        }
+
+        $mfConfigFile = join-path $modulePath $configFile
+        if(!(test-path $mfConfigFile))
+        {
+            throw 'Err: No Moduleforge Config File found. Please check your module path'
+        }
+
+        $moduleAzdFolder = join-path $modulePath $azdFolder
+
+    }
+    
+    process{
+        if(!(test-path $moduleAzdFolder)){
+            write-verbose "$moduleAzdFolder does not exist, creating"
+            new-item -ItemType Directory -Path $moduleAzdFolder
+        }
+
+        $childItemSource = get-childitem $resourceFolderAzd -Recurse
+        $childItemSource.foreach{
+            write-verbose "Checking file: $($_.name)"
+            $destinationPath = $_.FullName.replace($resourceFolderAzd,$moduleAzdFolder)
+            write-verbose 'DestinationPath: $destinationPath'
+            if(test-path $destinationPath){
+                if($force)
+                {
+                    write-warning "Overwrite $destinationPath"
+                    copy-item -Path $_.FullName -Destination $destinationPath -Force
+                }else{
+                    write-warning "Skipping $destinationPath as it exists"
+                }
+            }else{
+                write-verbose "Copying $($_) to $destinationPath"
+                copy-item -Path $_.FullName -Destination $destinationPath
+            }
+        }
+    }
+}
